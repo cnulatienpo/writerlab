c448f9fe8d5a47290a7b004fdeb294001bd16edb
cnulatienpo <119638971+cnulatienpo@users.noreply.github.com>
2025-07-19 19:20:55 +0000

    m

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/definition/[slug]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/definition/[slug].js"
     ]
   },
   "ampFirstPages": []
