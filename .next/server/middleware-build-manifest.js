c6ef8950df625483c6f5b55fe3c81edf90b0c49e
cnulatienpo <119638971+cnulatienpo@users.noreply.github.com>
2025-08-08 19:21:38 +0000

    n

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/deviousness": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/deviousness.js"
     ]
   },
   "ampFirstPages": []
